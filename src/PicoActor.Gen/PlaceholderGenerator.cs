5853b391b5cdf054b763251b486ce80036006b6c
Mutudu <aeondxf@live.com>
2026-08-06 18:00:12 +0800

    chore(gen): remove placeholder generator replaced by ActorSubscriberGenerator

--- src/PicoActor.Gen/PlaceholderGenerator.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.CodeAnalysis;
-
-namespace PicoActor.Gen;
-
-[Generator(LanguageNames.CSharp)]
-public sealed class PlaceholderGenerator : IIncrementalGenerator
-{
-    public void Initialize(IncrementalGeneratorInitializationContext context) { }
-}
